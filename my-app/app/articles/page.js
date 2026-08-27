f55081c0c02978aeeec79a70e9fb7251ce5759d8
baker123bakerhallak <bake.hallak.1996@gmail.com>
2026-08-27 13:33:38 +0300

    rebuild

--- my-app/app/articles/page.js
+++ my-app/app/articles/page.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Articles() {
   return (
     <div className="flex justify-center items-center h-screen">
-      <Link href={"./posts"} className="">
+      <Link href="/posts" className="">
         <button className="bg-amber-300 text-white px-2 py-2   border-b-white rounded-2xl cursor-pointer flex hover: border-b-amber-800  transition-[0.2] text-shadow-emerald-500 justify-center items-center">
           Take Me To The Post Page
         </button>
--- my-app/app/posts/page.js
+++ my-app/app/posts/page.js
@@ -13,7 +13,7 @@ async function PostPage() {
     <div className="p-10 grid  grid-cols-2   ">
       {posts.map((post, index) => {
         return (
-          <Link href={`posts/${post.id}`} key={post.id}>
+          <Link href={`/posts/${post.id}`} key={post.id}>
             <div className="flex bg-green-400 text-black border-2 justify-start items-center flex-col gap-10 p-10">
               <h2 className="bg-red-300 p-5 rounded-2xl">{post.title}</h2>
               <p>{post.body}</p>
