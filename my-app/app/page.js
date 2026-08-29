e16513a3c5fdc33ffa311f067edc2370f4ffc052
baker123bakerhallak <baker.hallak.1996@gmail.com>
2026-08-29 09:20:46 +0300

    not thing change

--- my-app/app/page.js
+++ my-app/app/page.js
@@ -4,6 +4,8 @@ import { Suspense } from "react";
 
 export default function Home() {
   return (
-    <div className="flex relative flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black"></div>
+    <div className="flex relative flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <h2>Hello! </h2>
+    </div>
   );
 }
